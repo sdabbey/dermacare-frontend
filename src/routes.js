af7b0743610f2928f5b03d7e0e2c2d03c9f87496
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2025-03-21 01:50:52 +0000

    update

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import { lazy } from 'react';
 
 
 const LandingPage = lazy(() => import('./pages/landingpage/LandingPage'));
+const SignIn = lazy(() => import('./pages/auth/SignIn'));
+const SignUp = lazy(() => import('./pages/auth/SignUp'));
 const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
 const FindADoctor = lazy(() => import('./pages/findadoctor/FindADoctor'));
 const DoctorDetail = lazy(() => import('./pages/doctordetail/DoctorDetail'));
 
 const routes = [
   { path: '/', component: LandingPage},
+  { path: '/signup', component: SignUp },
+  { path: '/signin', component: SignIn},
   { path: '/dashboard', component: Dashboard },
   { path: '/find-a-doctor', component: FindADoctor },
   { path: '/find-a-doctor/:id', component: DoctorDetail },
